728646e1b0958bb1917a08a956343f214b015190
Borodin Dmitriy <demansen@yandex.ru>
2018-04-28 02:10:09 +0300

    changed users

--- src/main/java/com/database/db/DAO/UserDAO.java
+++ src/main/java/com/database/db/DAO/UserDAO.java
@@ -1,4 +1,77 @@
 package com.database.db.DAO;
 
+import com.database.db.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional
 public class UserDAO {
+    private final JdbcTemplate jdbc;
+
+    @Autowired
+    public UserDAO(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    private static UserMapper userMapper = new UserMapper();
+
+    public void insertUser(User u) {
+        jdbc.update("INSERT INTO users (about, email, fullname, nickname) VALUES (?, ?, ?, ?)",
+                    u.getAbout(), u.getEmail(), u.getFullname(), u.getNickname());
+
+    }
+
+    public User getUserByNickname(String nickname) {
+        try {
+            return jdbc.queryForObject("SELECT * FROM users WHERE nickname=?::citext", userMapper, nickname);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public User getUserByEmail(String email) {
+        try {
+            return jdbc.queryForObject("SELECT * FROM users WHERE email=?::citext", userMapper, email);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public List<User> getUsers(User u) {
+        try {
+            List<Object> parameters = new ArrayList<>();
+            parameters.add(u.getNickname());
+            parameters.add(u.getEmail());
+            return jdbc.query("SELECT * FROM users WHERE nickname=?::citext OR email=?::citext", parameters.toArray(), userMapper);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public void updateUser(User u) {
+        jdbc.update("UPDATE users SET about=COALESCE(?, about), email=COALESCE(?, email), fullname=COALESCE(?, fullname) WHERE nickname=?::citext",
+                    u.getAbout(), u.getEmail(), u.getFullname(), u.getNickname());
+
+    }
+
+
+    private static class UserMapper implements RowMapper<User> {
+        public User mapRow(ResultSet result, int rowNum) throws SQLException {
+            return new User(
+                    result.getString("about"),
+                    result.getString("email"),
+                    result.getString("fullname"),
+                    result.getString("nickname")
+            );
+        }
+    }
 }
--- src/main/java/com/database/db/controller/UserController.java
+++ src/main/java/com/database/db/controller/UserController.java
@@ -21,37 +21,34 @@ public class UserController {
         this.uDAO = uDAO;
     }
 
-    // Создание пользователя
     @PostMapping(path = "/{nickname}/create")
     public ResponseEntity createUser(@PathVariable(name = "nickname") String nickname, @RequestBody User u) {
         try {
-            u.setNickName(nickname);
+            u.setNickname(nickname);
             uDAO.insertUser(u);
             return ResponseEntity.status(HttpStatus.CREATED).body(u);
         } catch (DuplicateKeyException e) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(uDAO.getUsers(u));
+            List<User> result = uDAO.getUsers(u);
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(result);
         }
     }
 
-    // Получение информации о пользователе
     @GetMapping(path = "/{nickname}/profile")
     public ResponseEntity getUserProfile(@PathVariable(name = "nickname") String nickname) {
-        User u = uDAO.getUserByNick(nickname);
+        User u = uDAO.getUserByNickname(nickname);
         if (u == null)
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new String("No user with nickname " + nickname));
         return ResponseEntity.status(HttpStatus.OK).body(u);
     }
 
-    // Обновление информации о пользователе
     @PostMapping(path = "/{nickname}/profile")
     public ResponseEntity updateUser(@PathVariable(name = "nickname") String nickname, @RequestBody User u) {
         try {
-            u.setNickName(nickname);
-            if (uDAO.getUserByNick(nickname) == null)
+            u.setNickname(nickname);
+            if (uDAO.getUserByNickname(nickname) == null)
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new String("No user with nickname " + nickname));
             uDAO.updateUser(u);
-
-            return ResponseEntity.status(HttpStatus.OK).body(uDAO.getUserByNick(nickname));
+            return ResponseEntity.status(HttpStatus.OK).body(uDAO.getUserByNickname(nickname));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body(new String("Not unique email"));
         }
